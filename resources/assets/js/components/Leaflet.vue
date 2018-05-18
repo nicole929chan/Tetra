c97cda7c77d7166a07f0f12b82ec91ce049cab6b
Nicole <nicole929chan@gmail.com>
2018-05-18 15:44:51 +0000

    暫存leaflet vue

--- resources/assets/js/components/Leaflet.vue
+++ resources/assets/js/components/Leaflet.vue
@@ -23,6 +23,9 @@
     	},
     	mounted () {
     		this.initLeaflet(this.version.id)
+
+    		this.retrieveDraws()
+
     	},
     	methods: {
     		initLeaflet (versionId) {
@@ -80,6 +83,11 @@
     		drawCreated (event) {
     			var layer = event.layer;
 
+    			var LatLngs = JSON.stringify(layer.toGeoJSON());
+	            var leafletKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+	            layer.leafletKey = leafletKey;
+
 	            var markForm = L.DomUtil.create('div', 'row');
     			markForm.innerHTML = `<div class="form-group">
 					<textarea rows="5" class="form-control" id="mark-body"></textarea>
@@ -94,11 +102,6 @@
 
 	            this.drawnItems.addLayer(layer);
 
-	            var LatLngs = JSON.stringify(layer.toGeoJSON());
-	            var leafletKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-
-	            layer.leafletKey = leafletKey;
-	            
 	            var versionId = this.version.id;
 
 	            // 新增一筆mark
@@ -152,7 +155,7 @@
 				        })
 	            }
 
-				$("#mark-save-btn", markForm).click({
+		    	$("#mark-save-btn", markForm).click({
 			    	LatLngs: LatLngs,
 			    	leafletKey: leafletKey,
 			    	versionId: versionId
@@ -165,6 +168,11 @@
 			    		saveMark(LatLngs, leafletKey, versionId);
 			    	}
 			    })
+
+			    // window.bus.$on('updateLeaflet', (markId, body) => {
+			    // 	var selector = "textarea[name='" + leafletKey + "']"
+			    // 	$(selector, markForm).val('test')
+		    	// })
     		},
     		drawDeleted (event) {
     			var layers = event.layers;
@@ -173,9 +181,71 @@
 					window.bus.$emit('destroyMark', layer.markId, layer.leafletKey)
     			});
     		},
+    		retrieveDraws() {
+    			let end_point = axios.defaults.baseURL + `/marks/versions/${this.version.id}`
+    			
+    			let markForm = function (body) {
+		    		let form = L.DomUtil.create('div', 'row');
+		    		form.innerHTML = `<div class="form-group">
+						<textarea rows="5" class="form-control" id="mark-body">${body}</textarea>
+						<div class="custom-file">
+							<input type="file" class="custom-file-input" id="mark-file">
+							<label class="custom-file-label">Choose file</label>
+						</div>
+						<button id="mark-save-btn" class="btn btn-sm btn-outline-success">save</button>
+					</div>`;
 
+					return form;
+		    	}
 
+    			axios.get(end_point)
+    			    .then(response => {
+    			    	let marks = response.data
 
+    			    	// retrieve all marks to layer
+    			    	marks.forEach(mark => {
+    			    		let l_object = JSON.parse(mark.l_object)
+
+    			    		let layer = L.geoJSON(l_object)
+    			    		layer.markId = mark.id
+    			    		layer.leafletKey = mark.leaflet_key
+    			    		let form = markForm(mark.body)
+
+    			    		// bind it's own data for popup
+    			    		layer.bindPopup(form, {minWidth: 200}).addTo(this.mymap)
+    			    		
+    			    		$("#mark-save-btn", form).on('click', null, { 
+    			    			markId: mark.id, body: $("#mark-body").val(), file: $("input:file").val() }, 
+    			    			this.updateMarkToDB);
+    			    	})
+    			    })
+    			    .catch(error => {
+    			    	console.log(error)
+    			    })
+    		},
+    		updateMarkToDB (event) {
+    			console.dir(event)
+    			// let data = new FormData()
+
+		    	// if (file !== undefined) {
+		    	// 	let reader = new FileReader()
+		    	// 	reader.readAsDataURL(file)
+	    		// 	data.append('file_path', file)
+		    	// }
+
+	    		// data.append('body', body)
+	    		// data.append('_method', 'PATCH');
+
+       //      	let end_point = axios.defaults.baseURL + `/marks/${markId}`;
+
+			    // axios.post(end_point, data)
+			    //     .then(response => {
+			    //     	// window.bus.$emit('updateMark', response.data.mark)
+			    //     })
+			    //     .catch(error => {
+			    //     	console.log(error)
+			    //     })
+    		}
 
     	}
     	
